aafe3c149845e5c2760a08c630b5db6ebc0751c4
rajshekhar <rajshekhar@RJAGADI>
2023-06-28 12:42:14 +0530

    Add project files.

---
+++ BooksApi/BooksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BooksApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksApi
+{
+    public class BooksDbContext: DbContext
+    {
+        //private readonly DbContext _dbContext;
+
+        public BooksDbContext(DbContextOptions options): base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        
+    }
+}
---
+++ BooksApi/Controllers/BooksApiController.cs
@@ -0,0 +1,64 @@
+﻿using BooksApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksApiController : ControllerBase
+    {
+        private readonly BooksDbContext _dbContext;
+
+        public BooksApiController(BooksDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_dbContext.Books);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Book book)
+        {
+            if(!ModelState.IsValid || book == null)
+            {
+                return BadRequest();
+            }
+
+            _dbContext.Books.Add(book);
+            _dbContext.SaveChanges();
+            return Ok(book);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(Book book)
+        {
+            if(!ModelState.IsValid || book == null)
+            {
+                return BadRequest();
+            }
+
+            _dbContext.Books.Remove(book);
+            _dbContext.SaveChanges();
+            return Ok(book);
+        }
+
+        [HttpPut]
+        public IActionResult Put(Book book)
+        {
+            if(!ModelState.IsValid || book == null)
+            {
+                return BadRequest();
+            }
+
+            _dbContext.Books.Update(book);
+            _dbContext.SaveChanges();
+            return Ok(book);
+        }
+    }
+}
